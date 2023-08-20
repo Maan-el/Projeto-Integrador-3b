a231ec5a4580dd73c4992661e76852a7b5eb9d8d
Maan-el <mancordosa@gmail.com>
2023-08-20 16:46:21 -0300

    Explicação simples para as flags usadas

--- compile.sh
+++ compile.sh
@@ -1,7 +1,14 @@
-gcc -Wall -Wextra -fanalyzer -lm -O3 -march=native -lpthread -ggdb3 -fsanitize=undefined -fsanitize=address -fno-sanitize-recover=all -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fno-sanitize=null -fno-sanitize=alignment -fstack-clash-protection -fstack-protector-strong main.c # debug
-gcc -Wall -Wextra -fanalyzer -lm -O3 -march=native -lpthread -fsanitize=undefined -fsanitize-minimal-runtime main.c # release
+gcc -Wall -Wextra -fanalyzer -lm -O3 -march=native -pthread -ggdb3 -fsanitize=undefined -fsanitize=address -fno-sanitize-recover=all -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fno-sanitize=null -fno-sanitize=alignment -fstack-clash-protection -fstack-protector-strong main.c # debug
+gcc -Wall -Wextra -fanalyzer -lm -O3 -march=native -pthread -fsanitize=undefined -fsanitize-minimal-runtime main.c # release
 
-# -D_FORTIFY_SOURCE=2 <- Coisa para segurança, irrelevante nesse caso
-# -fsanitize=thread <- Para o caso de tentarmos fazer algo multi-thread
+
+# Flags para validação de código durante a compilação   -> -Wall -Wextra -fanalyzer
+# Incluir o math.h                                      -> -lm
+# Se não me engano inclui o threads.h                   -> -pthread
+# Optimizações do executável                            -> -O3 -march=native
+# Informação para depuração                             -> -ggdb3
+# Coisa para segurança, irrelevante nesse caso          -> -D_FORTIFY_SOURCE=2
+# Validadores de código usados em tempo de execução     -> -fsanitize=undefined -fsanitize=address -fno-sanitize-recover=all -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fno-sanitize=null -fno-sanitize=alignment -fstack-clash-protection -fstack-protector-strong
+# Para o caso de tentarmos fazer algo multi-thread      -> -fsanitize=thread
 
 # Precisa do libubsan e do libasan para validar UB durante a execução
